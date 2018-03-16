3adab671af638f24cc503b7d391bdd6657d9bd5f
Антон Иванив <antoniden@gmail.com>
2018-03-16 16:03:58 +0300

    Правило тругольника,умножение на число,вычитание,транспонирование,имена.
    Матрицы более не double,теперь они целочисленные.
    Подправил функцию транспонирования,теперь она работает)
    Изменил некторые имена методов,заменил "array" на "Matrix",ну что за класс у меня)
    Написал метод нахождения детерминанта по правилу треугольника(ну ясное дело для матриц 3х3),но еще не проверил правильность.
    Также написал методы умножение матрицы на число,и вычитание матриц.
    Как-то так)

--- Matrix.hxx
+++ Matrix.hxx
@@ -11,18 +11,18 @@ private:
     unsigned rows;
     unsigned cols;
     string name;
-    double **mtrx;
+    int **mtrx;
 
-    double** array2d(unsigned int rows,unsigned int cols){
+    int ** crMatrix(unsigned int rows, unsigned int cols){
 
-        mtrx = new double* [rows];
+        mtrx = new int* [rows];
         for(int k=0;k<rows;k++){
-            mtrx[k] = new double[cols];
+            mtrx[k] = new int[cols];
         }
         return mtrx;
     }
 
-    void freeArray2d(double **array,unsigned int rows){
+    void freeMatrix(int **array, unsigned int rows){
         for (int i = 0; i < rows; i++) {
                 delete array[i];
         }
@@ -33,31 +33,32 @@ public:
     Matrix(){                                                                                       //пустой констрктор;
 
     }
-    Matrix(unsigned int rows,unsigned int cols,string name): rows(rows),cols(cols),name(name){
-//        int F0=0;
-//        int F1=1;
-//        int Fn=0;
-        mtrx=array2d(rows,cols);
+
+    Matrix(unsigned int rows,unsigned int cols,string name): rows(rows),cols(cols),name(name){ // Конструктор принимающий cтроки,столбцы и имя
+        int F0=0;
+        int F1=1;
+        int Fn=0;
+        mtrx= crMatrix(rows, cols);
         for (int i = 0; i <rows ; i++) {
             for (int j = 0; j <cols; j++) {
                 mtrx[i][j]=i+j;
-//                F0=F1;
-//                F1=Fn;
-//                Fn=F0+F1;
+                F0=F1;
+                F1=Fn;
+                Fn=F0+F1;
             }
         }
-    }// Конструктор принимающий cтроки,столбцы и имя
-    Matrix(Matrix &matrix) {
+    }
+    Matrix(Matrix &matrix) {                                                    // Копирующий конструктор
         setName(matrix.getName());
         setRows(matrix.getRows());
         setCols(matrix.getCols());
-        mtrx=array2d(rows,cols);
+        mtrx= crMatrix(rows, cols);
         for (int i = 0; i <rows ; i++) {
             for (int j = 0; j < cols; j++) {
                 mtrx[i][j]=matrix.mtrx[i][j];
             }
         }
-    }                                                                   // Копирующий конструктор
+    }
 
     void printMt(){
         string name=getName();
@@ -122,12 +123,12 @@ public:
     Matrix transpose() {
         unsigned int rows = getRows();
         unsigned int cols = getCols();
-        double **mtrxC = copyMt(*this);
-        freeArray2d(mtrx,rows);
-        mtrx = array2d(cols, rows);
+        Matrix matrixc(*this);
+        freeMatrix(mtrx, rows);
+        mtrx = crMatrix(cols, rows);
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                mtrx[i][j] = mtrxC[j][i];
+                mtrx[i][j] = matrixc.mtrx[j][i];
             }
         }
             setRows(cols);
@@ -135,10 +136,10 @@ public:
             return *this;
     }
 
-    double** copyMt(Matrix &matrix){
+    int ** copyMt(Matrix &matrix){
         unsigned rows=matrix.rows;
         unsigned cols=matrix.cols;
-        double** mtrx1=array2d(rows,cols);
+        int ** mtrx1= crMatrix(rows, cols);
         for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 mtrx1[i][j] = matrix.mtrx[i][j];
@@ -152,7 +153,7 @@ public:
         setName(matrix.getName());
         setRows(matrix.getRows());
         setCols(matrix.getCols());
-        mtrx=array2d(rows,cols);
+        mtrx= crMatrix(rows, cols);
         for (int i = 0; i <rows ; i++) {
             for (int j = 0; j < cols; j++) {
                 mtrx[i][j]=matrix.mtrx[i][j];
@@ -161,6 +162,66 @@ public:
         return *this;
     }
 
+    int determinant(){
+        unsigned int rows=getRows();
+        unsigned int cols=getCols();
+        if(rows==3&&cols==3){
+            cout<< this->determaminat3x3();
+            return EXIT_SUCCESS;
+        }
+
+        //TODO Определитель
+    }
+    int determaminat3x3(){
+//        unsigned int rows=matrix.rows;
+//        unsigned int cols=matrix.cols;
+        int Temp1=mtrx[0][0];
+        int Temp2=mtrx[0][1];
+        int Temp3=mtrx[0][2];
+        int Temp4=mtrx[1][0];
+        int Temp5=mtrx[1][1];
+        int Temp6=mtrx[1][2];
+        int Temp7=mtrx[2][0];
+        int Temp8=mtrx[2][1];
+        int Temp9=mtrx[2][2];
+        int Temp;
+        Temp = Temp1 * Temp5 * Temp9 + Temp2 * Temp6 * Temp7 + Temp4 * Temp3 * Temp8 -
+               Temp3 * Temp5 * Temp7 - Temp2 * Temp4 * Temp9 - Temp1 * Temp6 * Temp8;
+        return Temp;
+   }
+
+
+    Matrix division(){
+        //TODO Деление
+    }
+
+    Matrix inverse(){
+        //TODO Обратная матрица
+    }
+    Matrix multiAT(int nmbr){
+        unsigned int rows=getRows();
+        unsigned int cols=getCols();
+        for(int k = 0; k < rows;k++){
+            for (int i = 0; i < cols; i++) {
+                mtrx[k][i]=mtrx[k][i]*nmbr;
+            }
+        }
+        return *this;
+        //TODO Умножение на число DONE
+    }
+    Matrix subtraction(Matrix &matrix){
+        unsigned int rows=getRows();
+        unsigned int cols=getCols();
+        if(rows!=matrix.rows||cols!=matrix.cols)
+            throw string("Matrix not equals\n");
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                mtrx[i][j]=mtrx[i][j]-matrix.mtrx[i][j];
+            }
+        }
+        //TODO Вычитание DONE
+    }
+    //TODO перегрузка операторов *,+,-,/,==
 //Getters
      unsigned int getRows() const {
         return rows;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "Matrix.hxx"
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Matrix matrix1(3,3,"x");
+    matrix1.printMt();
+    matrix1.determinant();
+    matrix1.transpose();
+    matrix1.printMt();
+    matrix1.determinant();
     return 0;
 }
